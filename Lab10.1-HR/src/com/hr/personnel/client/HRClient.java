8d8ec0532980f4242ba18810b29b03adcb56115d
Gage Hill <gage.hill@tlgcohort.com>
2023-10-26 11:36:20 -0600

    Lab10.1-HR (setRate() throws exception)

--- Lab10.1-HR/src/com/hr/personnel/client/HRClient.java
+++ Lab10.1-HR/src/com/hr/personnel/client/HRClient.java
@@ -9,7 +9,6 @@
 package com.hr.personnel.client;
 
 import com.hr.personnel.Department;
-import com.hr.personnel.Employee;
 import com.hr.personnel.HourlyEmployee;
 import com.hr.personnel.SalariedEmployee;
 import java.time.LocalDate;
@@ -19,14 +18,14 @@ import java.time.LocalDate;
  */
 class HRClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         // create Department object
         Department dept = new Department("Sales", "Seattle");
         System.out.println(dept);
 
         // add Employees to it
         dept.addEmployee(new SalariedEmployee("Jason",  LocalDate.of(1990, 8, 24), 2250.0));
-        dept.addEmployee(new HourlyEmployee("Julie",    LocalDate.of(2000, 2, 2), 35.0, 30.0));
+        dept.addEmployee(new HourlyEmployee("Julie",    LocalDate.of(2000, 2, 2), 35.0, 35.0));
         dept.addEmployee(new SalariedEmployee("Amilia", LocalDate.of(1999, 2, 6), 1250.0));
         dept.addEmployee(new HourlyEmployee("Logan",    LocalDate.of(2002, 6, 2), 40.0, 40.0));
 
