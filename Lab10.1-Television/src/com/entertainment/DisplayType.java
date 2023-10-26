76015cc26f66568b34d5682b49793144fccd9fb1
Gage Hill <gage.hill@tlgcohort.com>
2023-10-26 10:40:07 -0600

    Lab10.1-Television complete

---
+++ Lab10.1-Television/src/com/entertainment/DisplayType.java
@@ -0,0 +1,11 @@
+package com.entertainment;/*
+ * There are 5 instances of displayType listed below.
+ * These 5 displayType objects are references as:
+ * DisplayType.LED
+ * DisplayType.OLED
+ * ...
+ */
+
+public enum DisplayType {
+    LED, OLED, PLASMA, LCD, CRT
+}
---
+++ Lab10.1-Television/src/com/entertainment/Television.java
@@ -0,0 +1,126 @@
+package com.entertainment;
+
+/*
+ * Business class to model the workings of a television.
+ */
+public class Television {
+    // static variables - shared among all instances, can only touch other static variables
+    public static final int MIN_VOLUME = 0;
+    public static final int MAX_VOLUME = 100;
+    public static final String[] VALID_BRANDS = { "Samsung", "Sony", "LG", "Toshiba" }; // DO NOT do it like this, use enum className
+
+    private static int instanceCount = 0;
+
+    public static int getInstanceCount() {
+        return instanceCount;
+    }
+
+    // fields - attributes, properties
+    private String brand;   // default value for when client doesn't specify a value
+    private int volume;
+    private DisplayType display = DisplayType.LED;
+
+    // these are for muting behavior
+    private boolean isMuted; // provide a getter, only
+    private int oldVolume; // completely hidden, no get/set methods
+
+    // constructors
+    public Television() {
+        instanceCount++; // instanceCount = instanceCount + 1
+    }
+
+    public Television(String brand) throws IllegalArgumentException {
+        this();
+        setBrand(brand);
+    }
+
+    public Television(String brand, int volume) throws IllegalArgumentException {
+        this(brand);
+        setVolume(volume);
+    }
+
+    public Television(String brand, int volume, DisplayType display) throws IllegalArgumentException {
+        this.setBrand(brand); // use "this" when there are overlapping parameters
+        setVolume(volume);
+    }
+
+    // business "action" methods aka functions or operations
+    // What do Television objects do?
+    public void mute() {
+        if (!isMuted) { // not currently muted
+            oldVolume = getVolume();
+            setVolume(0);
+            isMuted = true;
+        }
+        else {         // we are currently muted, so restore the volume from oldVolume
+            setVolume(oldVolume); // back to 32
+            isMuted = false;
+        }
+    }
+
+    public DisplayType getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(DisplayType display) {
+        this.display = display;
+    }
+
+    public void turnOn() {
+        boolean isConnected = verifyInternetConnection();
+        System.out.println("Turning on your " + getBrand() + ", TV to volume " + getVolume());
+    }
+
+    public void turnOff() {
+        System.out.println("Shutting down... goodbye.");
+    }
+
+    // accessor methods - provide "controlled access" to the object's fields
+    public boolean isMuted() {
+        return isMuted;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String setBrand(String brand) {
+        return this.brand = brand;
+    }
+
+    /*public void setBrand(String brand) throws InvalidBrandException {
+     *  boolean vaild = brand;
+     *   if (brand.equals(VALID_BRANDS)) {
+     *   }
+     *   else() {
+     *       throw new InvalidBrandException(String.format("Invalid volume: %s Must be between %s and %s. \n", brand));
+     *   }
+     */
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) throws IllegalArgumentException {
+        if (MIN_VOLUME  <= volume && volume <= MAX_VOLUME) {
+            this.volume = volume;// Set the volume only if it's within the valid range
+            isMuted = false; // automatically unmuted
+        }
+        else {
+            throw new IllegalArgumentException(String.format("Invalid volume: %s. Must be between %s and %s.|n", volume, MIN_VOLUME, MAX_VOLUME));
+        }
+    }
+
+    private boolean verifyInternetConnection() {
+        return true; // this is a fake implementation
+    }
+
+    // toString()
+    public String toString() {
+        String volumeString = isMuted() ? "<muted>" : String.valueOf(getVolume());
+
+        return String.format("Television: brand=%s, volume=%s, display=%s", getBrand(), volumeString, getDisplay());
+
+        // "Television brand = " + getBrand() + ", volume " + volumeString + " Display: " + getDisplay();
+    }
+}
