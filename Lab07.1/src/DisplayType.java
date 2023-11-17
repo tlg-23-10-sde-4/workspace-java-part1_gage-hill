5461bd87be1adf19cf59f87264c6864a2fa37439
Gage Hill <gage.hill@tlgcohort.com>
2023-11-17 01:07:03 -0700

    Lab07.1 complete

--- Lab07.1/src/DisplayType.java
+++ Lab07.1/src/DisplayType.java
@@ -1,11 +1,36 @@
-/*
- * There are 5 instances of displayType listed below.
- * These 5 displayType objects are references as:
- * DisplayType.LED
- * DisplayType.OLED
- * ...
- */
-
 enum DisplayType {
-    LED, OLED, PLASMA, LCD, CRT
+    LED(1920,1080), // these are parameters, parameters are passed to the constructor
+    OLED(3840, 2160),
+    PLASMA(1280,720),
+    LCD(1920,1080),
+    CRT(340, 460);
+
+    private final int resolutionWidth;
+    private final int resolutionHeight;
+
+    // Enum constructor
+    DisplayType(int width, int height) {
+        this.resolutionWidth = width;
+        this.resolutionHeight = height;
+    }
+
+    // Getter methods
+    public int getResolutionWidth() { // getter methods retrieve declared values, 'width, height'
+        return resolutionWidth;
+    }
+
+    public int getResolutionHeight() {
+        return resolutionHeight;
+    }
 }
+/*
+ * used to define a set of constants
+ * there are 5 instances or constants of displayType and each represents a unique type of display
+ * reference example: 'DisplayType.LED'
+ * can only take one of a limited set of predefined values
+ *
+ * Advanced Enum Features:
+ * _enums can have attributes, methods(private), and CTORs
+ * _accessor methods like getter/setter's
+ * _'switch' statements are used by Enum's, they execute different code blocks depending on it's value
+ */
--- Lab07.1/src/Television.java
+++ Lab07.1/src/Television.java
@@ -1,51 +1,98 @@
-/*
- * Business class to model the workings of a television.
- */
 class Television {
     // static variables - shared among all instances, can only touch other static variables
     public static final int MIN_VOLUME = 0;
     public static final int MAX_VOLUME = 100;
-    public static final String[] VALID_BRANDS = { "Samsung", "Sony", "LG", "Toshiba" }; // DO NOT do it like this, use enum className
+    private static int instanceCount = 0; // tracks the number of instances created
 
-    private static int instanceCount = 0;
+    // Enum for valid brands
+    public enum Brand {
+        SAMSUNG,
+        SONY,
+        LG,
+        TOSHIBA
+    }
 
+    // instance fields (attributes, properties)
+    private Brand brand;
+    private int volume;
+    private DisplayType display;
+    private boolean isMuted;
+    private int oldVolume; // for managing mute state
+
+    // constructors
+    // instanceCount = instanceCount + 1
+    public Television() {
+        instanceCount++;
+    }
+
+    public Television(Brand brand) {
+        this();
+        this.brand = brand;
+    }
+
+    public Television(Brand brand, int volume, DisplayType display) {
+        this(brand); // use 'this' when there are overlapping parameters
+        setVolume(volume);
+        this.display = display;
+    }
+
+    // 'static' method
     public static int getInstanceCount() {
         return instanceCount;
     }
 
-    // fields - attributes, properties
-    private String brand;   // default value for when client doesn't specify a value
-    private int volume;
-    private DisplayType display = DisplayType.LED;
-
-    // these are for muting behavior
-    private boolean isMuted; // provide a getter, only
-    private int oldVolume; // completely hidden, no get/set methods
-
-    // constructors
-    public Television() {
-        instanceCount++; // instanceCount = instanceCount + 1
-    }
-
-    public Television(String brand) {
-    }
-
-    public Television(String brand, int volume, DisplayType display) {
-        this.setBrand(brand); // use "this" when there are overlapping parameters
-        setVolume(volume);
-    }
-
-    // business "action" methods aka functions or operations
-    // What do Television objects do?
+    // business 'action' methods aka functions or operations
+    // What do Television 'objects' do?
+    // 'if/else' volume rang checking validation
     public void mute() {
         if (!isMuted) { // not currently muted
             oldVolume = getVolume();
-            setVolume(0);
+            setVolume(MIN_VOLUME);
             isMuted = true;
         }
-        else {         // we are currently muted, so restore the volume from oldVolume
+        else {          // we are currently muted, so restore the volume from oldVolume
             setVolume(oldVolume); // back to 32
             isMuted = false;
+        }
+    }
+
+    public void turnOn() {
+        if (verifyInternetConnection()) {
+            System.out.println("Turning on your " + this.brand + " TV to volume " + this.volume);
+        }
+        else {
+            System.out.println("Failed to turn on TV: No internet connection.");
+        }
+    }
+
+    public void turnOff() {
+        System.out.println("Shutting off... goodbye.");
+    }
+
+    // accessor methods - provide 'controlled access' to the object's fields
+    public boolean isMuted() {
+        return isMuted;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        if (MIN_VOLUME  <= volume && volume <= MAX_VOLUME) {
+            this.volume = volume;// set the volume only if it's within the valid range
+            isMuted = false; // automatically unmuted
+        }
+        else {
+            System.out.printf("Invalid volume: %s. Must be between %s and %s.\n", volume, MIN_VOLUME, MAX_VOLUME);
         }
     }
 
@@ -57,53 +104,14 @@ class Television {
         this.display = display;
     }
 
-    public void turnOn() {
-        boolean isConnected = verifyInternetConnection();
-        System.out.println("Turning on your " + getBrand() + ", TV to volume " + getVolume());
-    }
-
-    public void turnOff() {
-        System.out.println("Shutting down... goodbye.");
-    }
-
-    // accessor methods - provide "controlled access" to the object's fields
-    public boolean isMuted() {
-        return isMuted;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public String setBrand(String brand) {
-        return this.brand = brand;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        if (MIN_VOLUME  <= volume && volume <= MAX_VOLUME) {
-            this.volume = volume;// Set the volume only if it's within the valid range
-            isMuted = false; // automatically unmuted
-        } else {
-            System.out.printf("Invalid volume: %s. Must be between %s and %s.|n", volume, MIN_VOLUME, MAX_VOLUME);
-
-            // System.out.println("Invalid volume: " + volume + ". " + "Valid range is [" + MIN_VOLUME + " - " + MAX_VOLUME + "].");
-        }
-    }
-
     private boolean verifyInternetConnection() {
-        return true; // this is a fake implementation
+        // placeholder for method that would check internet connectivity
+        return true;
     }
 
-    // toString()
+    // provides a string representation of 'Television' object
     public String toString() {
-        String volumeString = isMuted() ? "<muted>" : String.valueOf(getVolume());
-
+        String volumeString = isMuted() ? "<muted>" : String.valueOf(volume);
         return String.format("Television: brand=%s, volume=%s, display=%s", getBrand(), volumeString, getDisplay());
-
-        // "Television brand = " + getBrand() + ", volume " + volumeString + " Display: " + getDisplay();
     }
 }
