d49c1a58ba5017d63f2dd4b627a281f54ece36f0
Gage Hill <gage.hill@tlgcohort.com>
2023-11-16 21:44:53 -0700

    Added notes

--- Lab01.1/src/HelloWorld.java
+++ Lab01.1/src/HelloWorld.java
@@ -1,14 +1,19 @@
-/*
- * Classic "hello world" application in Java.
- * It is a standalone application, i.e., runs directly on the JVM.
- * It is what we call a "main-class," i.e., a class definition with the main() method.
-*/
 class HelloWorld {
-
-    // entry point or starting point of every standalone Java application
     public static void main(String[] args) {
         System.out.println("Hello from IntelliJ!");
         System.out.println("I like IntelliJ better than bare metal programming.");
         System.out.println("TLG-SDE Pre-IT Apprenticeship Class of 24'");
     }
 }
+/*
+ * starting-point of every standalone Java application
+ * PascalCase naming convention
+ * 'public' assessable from anywhere
+ * 'static' doesn't require an instance of 'HelloWorld' to be called
+ * main method takes a (String[] args) as an argument
+ * 'sout' output
+ *
+ * Potential:
+ * _using constants
+ * _using a message management system
+ */
