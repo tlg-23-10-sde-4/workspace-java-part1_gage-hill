1cf8e7eb3da9d3a6615c407554405adfa6af3f3b
Gage Hill <gage.hill@tlgcohort.com>
2023-11-17 00:28:18 -0700

    Lab12.1 refactored writePoem() to write a file instead of read a file

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -1,66 +1,32 @@
-/*
- * This code is sample code, provided AS-IS, and we make NO
- * warranties as to its correctness or suitability for any purpose.
- *
- * We hope that it's useful to you.  Enjoy.
- * Copyright LearningPatterns Inc.
- */
-
 package com.poetry.io;
 
 import java.io.*;
 
 public class PoemClient {
 
-    /**
-     * To run one method at a time, uncomment the call to the one you want to execute.
-     */
     public static void main(String[] args) {
-        // readPoem();
+        readPoem();
         writePoem();
     }
 
-    /**
-     * TASK: read all the lines in file 'famous-poem.txt' and print them.
-     *
-     * Avoid the temptation to open the file in the IDE first.
-     * You'll see the poem once you get your code working(!)
-     *
-     * This file is in the module's root directory, which is the current / working
-     * directory at runtime.  Therefore, the path to the file is just the filename.
-     *
-     * Use a BufferedReader wrapped around a FileReader.
-     * The try-with-resources below allows you to initialize the stream and auto-close it.
-     */
     private static void readPoem() {
         try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
             String line;
-            while ( (line = reader.readLine()) != null ) { // null means EOF
+            while ((line = reader.readLine()) != null ) { // null means EOF
                 System.out.println(line);
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println();
     }
 
-    /**
-     * TASK: write a Haiku to file 'haiku.txt'.
-     *
-     * A Haiku is a 3-line poem with the following characteristics:
-     *  1st line is 5 syllables
-     *  2nd line is 7 syllables
-     *  3rd line is 5 syllables
-     *
-     * Use a PrintWriter wrapped around a FileWriter.
-     * Use a try-with-resources to initialize the stream and auto-close it.
-     */
     private static void writePoem() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("haiku.txt"))) {
+        String poem = "Whispers in the breeze, \nLeaves dance under moonlit sky, \nNature's quiet song.";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("haiku.txt"))) {
             String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+            writer.write(poem);
         }
         catch (IOException e) {
             e.printStackTrace();
