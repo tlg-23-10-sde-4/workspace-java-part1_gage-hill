33613fc90dbb3b5a745f071418aef810e4c02254
Gage Hill <gage.hill@tlgcohort.com>
2023-11-17 01:27:05 -0700

    Lab07.2-Client added operateTelevision() helper method, removed redundant toString() calls

--- Lab07.2-Client/src/com/entertainment/client/TelevisionClient.java
+++ Lab07.2-Client/src/com/entertainment/client/TelevisionClient.java
@@ -1,26 +1,11 @@
-/*
- * This code is sample code, provided as-is, and we make no
- * warranties as to its correctness or suitability for any purpose.
- *
- * We hope that it's useful to you.  Enjoy.
- * Copyright LearningPatterns Inc.
- */
-
 package com.entertainment.client;
 
 import com.entertainment.DisplayType;
 import com.entertainment.Television;
 import com.entertainment.util.Useful;
 // import com.entertainment.util.Useful; // this is not visible cannot use
-
-/**
- * Tester class, or "client" class, with main().
- * It creates a few instances of com.entertainment.Television and gives them a test-drive,
- * i.e., sets their properties and calls their methods.
- */
 class TelevisionClient {
-
-    // ENTRY-POINT or starting point for a standalone Java application
+    // starting point for a standalone Java application
     public static void main(String[] args) {
         // Useful is a public class in another package - good, because I need to use it here
         Useful.help();
@@ -31,31 +16,32 @@ class TelevisionClient {
         // create an instance of com.entertainment.Television, set its properties, then call methods on it
         // here, we use the no-arg ctor and set the properties individually, via the setters
         Television tv1 = new Television();
-        tv1.setBrand("Samsung");
+        tv1.setBrand(Television.Brand.valueOf("Samsung"));
         tv1.setVolume(32);
         tv1.setDisplay(DisplayType.OLED);
-        tv1.turnOn();
-        tv1.turnOff();
-        System.out.println();
+        operateTelevision(tv1);
 
+        // second instance of 'Television'
         // use the 3-arg brand-volume-display constructor
-        Television tv2 = new Television("Sony", 50, DisplayType.PLASMA);
+        Television tv2 = new Television(Television.Brand.SONY, 50, DisplayType.PLASMA);
         tv2.turnOn();
         tv2.turnOff();
-        System.out.println();
+        operateTelevision(tv2);
 
+        // third instance of 'Television'
         // use the 1-arg brand constructor - volume will default to 0 and display to LED
-        Television tv3 = new Television("LG");
-        tv3.turnOn();
-        tv3.turnOff();
+        Television tv3 = new Television(Television.Brand.LG);
         System.out.println();
-
-        // try out their toString() methods
-        System.out.println(tv1.toString());
-        System.out.println(tv2);  // toString() automatically called
-        System.out.println(tv3);
-        System.out.println();
+        operateTelevision(tv3);
 
         System.out.println(Television.getInstanceCount() + " instances created");
+    }
+
+    // helper method to operate and display information about a Television
+    public static void operateTelevision(Television tv) {
+        tv.turnOn();
+        tv.turnOff();
+        System.out.println(tv);
+        System.out.println();
     }
 }
